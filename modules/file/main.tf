5940b8b90ef23a3322af6db93aebd4456274d0c6
Victor M. Varela <vmvarela@hiberus.com>
2025-03-22 17:31:06 +0000

    docs: file sub-module link

---
+++ modules/file/main.tf
@@ -0,0 +1,13 @@
+resource "github_repository_file" "this" {
+  repository                      = var.repository
+  file                            = var.file
+  content                         = var.from_file != null ? file(var.from_file) : var.content
+  branch                          = var.branch
+  commit_author                   = var.commit_author
+  commit_email                    = var.commit_email
+  commit_message                  = var.commit_message
+  overwrite_on_create             = var.overwrite_on_create
+  autocreate_branch               = var.autocreate_branch
+  autocreate_branch_source_branch = var.autocreate_branch_source_branch
+  autocreate_branch_source_sha    = var.autocreate_branch_source_sha
+}
